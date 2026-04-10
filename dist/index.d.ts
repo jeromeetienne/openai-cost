a7dbb71b7e42d1c886d016becc68284fb6cfcfcf
Jerome Etienne <jerome.etienne@gmail.com>
2026-04-10 18:28:07 +0200

    refactor: remove obsolete TypeScript definitions and implementation for OpenAICallTracker and OpenAiCostCalculator

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { OpenAICallTracker } from "./openai_call_tracker";
-export type { OpenAICallTrackerCallback } from "./openai_call_tracker";
-export { OpenAiCostCalculator, pricingPerModel } from "./openai_cost_calculator";
-export type { OpenAiCostResponse, PricingForModel, PricingPerModel, PriorityType, } from "./openai_cost_calculator";
